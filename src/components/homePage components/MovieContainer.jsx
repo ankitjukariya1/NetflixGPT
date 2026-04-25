a55bb37185c86208550578e934640b5d0392e5fd
Ankit jukariya <ankujukariya@gmail.com>
2026-04-25 23:52:44 +0530

    minor improvement in ui

--- src/components/homePage components/MovieContainer.jsx
+++ src/components/homePage components/MovieContainer.jsx
@@ -1,13 +1,14 @@
 
+import { useEffect } from "react"
 import MovieCard from "../common/MovieCard"
 import { MovieContainerShimmer } from "./MovieContainerShimmer"
 
 
 const MovieContainer = ({ data }) => {
-   const Movies = data.data && data.data?.results
-   console.log(Movies)
+    
+     const Movies =  data.data?.results
    return Movies?<div className="ml-2 z-40 overflow-hidden popular scrollbar-hide mt-4 gap-2 flex overflow-x-auto  ">
-      {Movies && Movies.map((m) => {
+      { Movies.map((m) => {
          return <MovieCard movie={m} key={m.id} ></MovieCard>
       })}
    </div>:<MovieContainerShimmer></MovieContainerShimmer>
