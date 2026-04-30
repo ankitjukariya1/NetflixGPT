ac869f56d4a1a1840d85c072cab6b681962c009b
Ankit jukariya <ankujukariya@gmail.com>
2026-04-30 23:24:30 +0530

    fixed some bug

--- src/components/common/MovieCard.jsx
+++ src/components/common/MovieCard.jsx
@@ -15,9 +15,9 @@ const MovieCard = ({ movie }) => {
         })
      const handleMovieClick = ()=>{
         setMovieClick(true);
-     }
+     }  
   console.log(movieVideoQuery?.data)
-  return <div onClick={handleMovieClick} className={`movieCard hover:cursor-pointer w-20 md:w-40 lg:w-60  shrink-0 rounded-2xl transition-all aspect-[2/3]  duration-500 hover:scale-102 ${loading&&'bg-white/20 '}`}>
+  return <div onClick={handleMovieClick} className={`movieCard hover:cursor-pointer w-20 md:w-40 lg:w-60  shrink-0 rounded-2xl transition-all aspect-2/3  duration-500 hover:scale-102 ${loading&&'bg-white/20 '}`}>
     <div className="poster "  ><img onLoad={() => setloading(false)}  className="rounded-2xl " src={`${TMDB_IMG_PREFIX}/w300/${movie?.poster_path
       }`} alt="movie image" /></div>
 
